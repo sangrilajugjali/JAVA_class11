6da18cc24917885a4b8c4538740c1a25b6e7798d
sangrilamagar <magarsangrila@gmail.com>
2025-11-23 14:45:51 +0545

    progarm

---
+++ day_7/Square.java
@@ -0,0 +1,10 @@
+package day_7;
+
+class Square{
+    public static void main (String[]agrs){
+        for(int n=1;n<=10;n++){
+            int Square=n*n;
+            System.out.println("Square of " + n + " is: " + Square);
+        }
+    }
+}
